1c68268108bf1e4c0f1debcef259ab02287bde1e
Bey <bcmm@BCMM-PC>
2019-05-14 19:18:10 +0200

    Project beginning

---
+++ HelloWold/src/helloworld/mainClass.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+	}
+
+}
